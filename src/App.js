3e2e88def56bedeea6c086831bb4c92a30d083d7
Mudassir Saeed <mudassirsaeed23@gmail.com>
2021-09-05 19:39:12 +0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';  
+import { Component} from "react";
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/menuComponent';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
     </div>
   );
 }
