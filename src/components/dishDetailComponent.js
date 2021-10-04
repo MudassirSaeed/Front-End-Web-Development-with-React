0d071813d8e27572215e2e82562c871d1fce3f50
Mudassir Saeed <mudassirsaeed23@gmail.com>
2021-10-04 15:59:08 +0500

    redux thunk

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import {
     Button, Modal, ModalBody, ModalHeader, Label, Row, Col
 } from "reactstrap";
-
+import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
@@ -47,6 +47,7 @@ function RenderComments({ dish, comments, addComment, dishId }) {
             </li>
         )
     })
+    
     if (comments != null)
         return (
             <div className="col-12 col-md-12 m-1">
@@ -71,10 +72,27 @@ function RenderComments({ dish, comments, addComment, dishId }) {
 
 const Detail = (props) => {
 
-    console.log('DishdetailComponent render invoked');
-
-    const dish = props.dish;
-    if (dish == null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    //else if (props.dish != null)
+    //const dish = props.dish;
+    else if (props.dish == null) {
         return (
             <div></div>
         );
